aa1b754b65eeb0053dc36e441036c89805b84503
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2024-06-15 15:51:48 +0200

    cleanup

--- lib/mapaum/client.go
+++ lib/mapaum/client.go
@@ -2,53 +2,38 @@ package mapaum
 
 import (
 	"fmt"
-	"io"
 	"log"
-	"net/http"
-	"net/http/cookiejar"
 	"net/url"
+
+	"github.com/headzoo/surf"
+	"github.com/headzoo/surf/browser"
 )
 
 type Client struct {
-	client http.Client
-	jar    http.CookieJar
+	bow       *browser.Browser
+	viewState string
 }
 
 func New() (*Client, error) {
-	jar, err := cookiejar.New(nil)
+	bow := surf.NewBrowser()
+
+	err := bow.Open("https://mapa.um.warszawa.pl/mapaApp1/faces/oferty/ofertySprzedaz.xhtml?lang=pl")
 	if err != nil {
-		return nil, err
+		panic(err)
 	}
-	client := &Client{
-		client: http.Client{},
-		jar:    jar,
+
+	var viewStateNode = bow.Dom().Find("input[name=javax\\.faces\\.ViewState]")
+
+	viewState, exists := viewStateNode.First().Attr("value")
+	if !exists {
+		panic("viewstate not exists")
 	}
+
+	client := &Client{bow: bow, viewState: viewState}
 	return client, nil
 }
 
-func (r *Client) Init() error {
-	resp, err := r.client.Get("https://mapa.um.warszawa.pl/mapaApp1/mapa?service=nieruchomosci")
-
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("status %v", resp.StatusCode)
-	}
-
-	bodyBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-	bodyString := string(bodyBytes)
-	log.Println(bodyString)
-
-	return nil
-}
-
-func (r *Client) ListRealEstates(page int) ([]RealEstate, error) {
+func (r *Client) ListRealEstates(page int, pageSize int) ([]RealEstate, error) {
 	form := url.Values{}
 	form.Add("javax.faces.partial.ajax", "true")
 	form.Add("javax.faces.source", "form1:dataTable1")
@@ -56,8 +41,8 @@ func (r *Client) ListRealEstates(page int) ([]RealEstate, error) {
 	form.Add("javax.faces.partial.render", "form1:dataTable1")
 	form.Add("form1:dataTable1", "form1:dataTable1")
 	form.Add("form1:dataTable1_pagination", "true")
-	form.Add("form1:dataTable1_first", "0")
-	form.Add("form1:dataTable1_rows", "10")
+	form.Add("form1:dataTable1_first", fmt.Sprint(page*pageSize))
+	form.Add("form1:dataTable1_rows", fmt.Sprint(pageSize))
 	form.Add("form1:dataTable1_encodeFeature", "true")
 	form.Add("form1", "form1")
 	form.Add("form1:dataTable:j_idt11:filter", "")
@@ -67,23 +52,14 @@ func (r *Client) ListRealEstates(page int) ([]RealEstate, error) {
 	form.Add("form1:dataTable:j_idt23:filter", "")
 	form.Add("form1:dataTable1_selection", "8712")
 	form.Add("form1:dataTable1_scrollState", "0,0")
-	form.Add("javax.faces.ViewState", "6458804742846979572:2467599149130526145")
-	resp, err := r.client.PostForm("https://mapa.um.warszawa.pl/mapaApp1/faces/oferty/ofertySprzedaz.xhtml", form)
+	form.Add("javax.faces.ViewState", r.viewState)
+
+	err := r.bow.PostForm("https://mapa.um.warszawa.pl/mapaApp1/faces/oferty/ofertySprzedaz.xhtml", form)
 	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("status %v", resp.StatusCode)
+		log.Fatal(err)
 	}
 
-	bodyBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	bodyString := string(bodyBytes)
-	log.Println(bodyString)
+	log.Println(r.bow.Body())
 
-	return []RealEstate{{}}, nil
+	return []RealEstate{}, nil
 }
